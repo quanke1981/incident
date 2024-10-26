8e3d07daf3aa011e4350af632c42197e4ef3160e
quanke <14915148@qq.com>
2024-10-26 20:45:27 +0800

    update

--- incident-service/src/main/java/com/example/incident/controller/IncidentRestController.java
+++ incident-service/src/main/java/com/example/incident/controller/IncidentRestController.java
@@ -3,6 +3,9 @@ package com.example.incident.controller;
 import com.example.incident.model.Incident;
 import com.example.incident.model.PageInfo;
 import com.example.incident.service.IncidentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,26 +20,54 @@ public class IncidentRestController {
     }
 
     @GetMapping("/incidents/{id}")
+    @Operation(summary = "Get an incident", description = "Get an incident by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful"),
+            @ApiResponse(responseCode = "404", description = "Incident is not found by given id"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     public ResponseEntity<Incident> getIncident(@PathVariable Long id) {
         return ResponseEntity.ok(incidentService.getIncident(id));
     }
 
     @GetMapping("/incidents")
+    @Operation(summary = "Get incidents", description = "Get incidents by pages")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     public ResponseEntity<PageInfo<Incident>> getAllIncidents(Pageable pageable) {
         return ResponseEntity.ok(incidentService.getIncidents(pageable));
     }
 
     @PostMapping("/incidents")
+    @Operation(summary = "Create an incident", description = "Create an incident")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     public ResponseEntity<Incident> createIncident(@RequestBody Incident incident) {
         return ResponseEntity.ok(incidentService.createIncident(incident));
     }
 
     @PutMapping("/incidents/{id}")
+    @Operation(summary = "Update an incident", description = "Update an incident by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful"),
+            @ApiResponse(responseCode = "404", description = "Incident with given id doesn't exist"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     public ResponseEntity<Incident> updateIncident(@RequestBody Incident incident, @PathVariable Long id) {
         return ResponseEntity.ok(incidentService.updateIncident(incident, id));
     }
 
     @DeleteMapping("/incidents/{id}")
+    @Operation(summary = "Delete an incident", description = "Delete an incident by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful"),
+            @ApiResponse(responseCode = "404", description = "Incident with given id doesn't exist"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     public ResponseEntity<Void> deleteIncident(@PathVariable Long id) {
         incidentService.deleteIncident(id);
         return ResponseEntity.ok().build();
